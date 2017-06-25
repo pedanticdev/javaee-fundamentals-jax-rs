a720571b59026ba562942d0bf9652bd80297e154
sinaisix@gmail.com <sinaisix@gmail.com>
2017-06-25 13:09:27 +0000

    Initial Commit

---
+++ src/main/java/com/pedantic/config/RootResource.java
@@ -0,0 +1,7 @@
+package com.pedantic.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+@ApplicationPath("api/v1")
+public class RootResource extends Application {
+}
